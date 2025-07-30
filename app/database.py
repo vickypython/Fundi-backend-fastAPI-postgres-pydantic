ae2fac937fe751b0ac87e16b83301cf8d31def48
vickypython <vickymlucky6@gmail.com>
2025-07-30 10:15:25 +0300

    modified the database file

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.ext.asyncio import create_async_engine
 DATABASE_URL='sqlite:///./fundi_app.db'
 engine=create_engine(DATABASE_URL)
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bing=engine)
+engine_new=create_async_engine(DATABASE_URL,pool_size=30)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bing=engine,)
 Base=declarative_base()
 def get_db():
     db=SessionLocal()
@@ -12,4 +13,7 @@ def get_db():
         yield db
     finally:
         db.close()
+async def get_dbs():
+    with SessionLocal as session:
+        yield session
         
